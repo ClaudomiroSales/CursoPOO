f950cf083600080b8a002018ba77501f10407739
ClaudomiroSales <claudomiro.sales@gmail.com>
2021-11-17 15:54:46 -0300

    Criado o main e atualizdo o gitignore com json

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+* Multiple line
+* comment
+*/
+#include<iostream>
+
+//Single line comment
+using std::cout;
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout << "Hello World!\n";
+
+   
+   return 0;
+}
